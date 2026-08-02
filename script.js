fb7f3df669f9194d514696be06764ff9d213b3f8
KONA BHARGAVI <bhargavikona07@gmail.com>
2026-08-02 11:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('Portfolio loaded');
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Bhargavi Portfolio Loaded Successfully");
+
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+        section.style.opacity = "0";
+        section.style.transform = "translateY(30px)";
+        section.style.transition = "all 0.8s ease";
+    });
+
+
+    function showSections() {
+
+        sections.forEach(section => {
+
+            const position = section.getBoundingClientRect().top;
+            const screenHeight = window.innerHeight;
+
+
+            if(position < screenHeight - 100) {
+
+                section.style.opacity = "1";
+                section.style.transform = "translateY(0)";
+
+            }
+
+        });
+
+    }
+
+
+    window.addEventListener("scroll", showSections);
+
+    showSections();
+
+});
